05dead6824a2f567398ff3958adc1f68fc39d2cb
Chris Kokolios <kokolios1986@yahoo.gr>
2016-06-23 23:41:22 +0300

    initial setup, sample landing page

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app  = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.use(express.static('public'));
+
+app.set('view engine' , 'ejs');
+
+
+app.get('/' , (req, res ) => {
+    
+      res.render('landing');
+});
+
+
+app.listen(port , () => {
+    console.log("Server running on port " + port);
+});
