037165fe0ee65dfa0bcdbd776d1089366bec77f6
Chris Kokolios <kokolios1986@yahoo.gr>
2016-06-26 17:23:44 +0300

    pre add hearthstone api

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,65 @@
 const express = require('express');
-const router =   express.Router();
-const User =  require('../models/user');
+const router = express.Router();
+const User = require('../models/user');
 
 
 
 
-router.get('/' , (req,res) => {
-
-		User.find({})
-			.then(users => { res.send(users)})
-			.catch(err => {console.log(err)})
+router.get('/', (req, res) => {
+    //need to render a list of users somewhere, not sure yet
+    User.find({})
+        .then(users => { res.render('profiles/index', { profiles: users }) })
+        .catch(err => { console.log(err) })
 });
 
-router.get('/:userID', (req,res) => {
-	
-	let userId = req.params.userID;
+router.get('/:userID', (req, res) => {
+    //show the profile page	
+    let userId = req.params.userID;
 
-	User.findById(userId)
-		.then(user => {res.send(user)})
-		.catch(err => {console.log(err)})
+    User.findById(userId)
+        .then(user => { res.send(user) })
+        .catch(err => { console.log(err) })
 })
 
+router.get('/:userID/edit', (req, res) => {
+    let userId = req.params.userID;
+
+
+    User.findById(userId)
+        .then(user => {
+            console.log(user);
+            res.render('profiles/edit', { profile: user })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.put("/:userID", (req, res) => {
+
+    let userId = req.params.userID;
+    User.findByIdAndUpdate(userId, req.body.profile)
+        .then(user => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.delete("/:userID/" , (req,res) => {
+	let userId = req.params.userID;
+	User.findByIdAndRemove(userId)
+		.then(res.redirect('back'))
+		.catch(err => {
+			console.log(err);
+		})
+});
 
 
 
 
+//need to secure routes
 
 
 
