98286d0b18b09f6fd8d51a0ac00f65de0257cd38
kokoChris <kokolios1986@yahoo.gr>
2016-06-30 17:57:02 +0300

    working on trades

--- public/customjs/requestTrade.js
+++ public/customjs/requestTrade.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $('.container').on('click', '.tradeBtn', function() {
-        var tradeeId =  $(this).attr('value');
+//     $('.container').on('click', '.tradeBtn', function() {
+//         var tradeeId =  $(this).attr('value');
 
 
-        $.post('/trades', {id: tradeeId})
-            .done(function(data) {
-            	console.log(data.message)
-            });
-    });
-});
+
+
+        
+
+//         $.post('/trades', {id: tradeeId})
+//             .done(function(data) {
+//             	console.log(data.message)
+//             });
+//     });
+// });
--- routes/trades.js
+++ routes/trades.js
@@ -11,7 +11,7 @@ router.post('/', (req, res) => {
 
     let cardId = req.body.id;
     let buyerId =  req.user._id;
-     User.find().elemMatch('cards',{_id:cardId}).then( seller => {
+    User.find().elemMatch('cards',{_id:cardId}).then( seller => {
      	this.seller = seller[0];
      	this.seller.incomingTrades.push({
      		card: cardId,
@@ -41,17 +41,31 @@ router.post('/', (req, res) => {
  		console.log(err)
  	})
     
-    // find seller and creating incoming request
-   
-
-    
-
     res.send({message:'Trade Requested!!!',
 
 
     });
 
 })
+
+
+router.get('/new', (req,res) => {
+   let requestedCardId = req.query.requestedCardId;
+   
+   let findSeller = User.find().elemMatch('cards',{_id:requestedCardId})
+   let findBuyer = User.find({_id: req.user._id});
+
+   Promise.all([findSeller,findBuyer])
+   		.then(results => {
+   			let buyerCards = results[1][0].cards;
+   			let sellerCards = results[0][0].cards;
+   			let sellerCard = sellerCards.filter( card => {
+   				
+   				return card._id == requestedCardId
+   			})
+   			res.render('/trades/new',{buyerCards,sellerCard})
+   		})
+})	
 
 
 
